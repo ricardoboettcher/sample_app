aca202c82fcfd05dadaee1fc46e9406f505202ee
Ricardo Böttcher <ricardo-boettcher@gmx.de>
2012-10-19 16:39:57 +0200

    beginning 11.3.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,11 +33,6 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-  def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
-  end
-
   def following?(other_user)
     relationships.find_by_followed_id(other_user.id)
   end
@@ -48,6 +43,10 @@ class User < ActiveRecord::Base
 
   def unfollow!(other_user)
     relationships.find_by_followed_id(other_user.id).destroy
+  end
+
+  def feed
+    Micropost.from_users_followed_by(self)
   end
 
   private
